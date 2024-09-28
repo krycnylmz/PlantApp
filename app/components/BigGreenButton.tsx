fa71dc4b9f94183d3328e1e589624630d8e8ec61
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-09-28 17:31:24 +0300

    Update: button opacity changed as 0.8

--- app/components/BigGreenButton.tsx
+++ app/components/BigGreenButton.tsx
@@ -9,23 +9,23 @@ interface BigGreenButtonProps {
 
 const BigGreenButton: React.FC<BigGreenButtonProps> = ({ title, onPress, style }) => {
   return (
-    <View style={styles.container}>      
-    <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
-      <Text style={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress} style={[styles.button, style]} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    width:"100%",
-    paddingHorizontal:24,
+  container: {
+    width: "100%",
+    paddingHorizontal: 24,
     alignItems: 'center',
     justifyContent: 'center',
   },
   button: {
-    width:'100%',
+    width: '100%',
     backgroundColor: '#28AF6E',
     paddingVertical: 18,
     paddingHorizontal: 16,
@@ -36,8 +36,8 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#FFFFFF',
     fontSize: 15,
-    lineHeight:24,
-    letterSpacing:-0.24,
+    lineHeight: 24,
+    letterSpacing: -0.24,
     fontFamily: 'Rubik_500Medium',
   } as TextStyle, // Explicitly declare the type for TextStyle
 });
