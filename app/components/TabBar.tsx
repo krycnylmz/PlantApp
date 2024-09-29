e540aa8ca47b6966f6400c9a55a90a7e5c04d386
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-09-29 21:55:48 +0300

    create(component): TabBar

---
+++ app/components/TabBar.tsx
@@ -0,0 +1,15 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+const TabBar = () => {
+  return (
+    <View style={tabBarStyles.container}>
+      <Text>tabbar</Text>
+    </View>
+  );
+}
+
+const tabBarStyles = StyleSheet.create({
+  container:{}
+})
+export default TabBar
