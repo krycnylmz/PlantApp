3bb788f1395510b7ea2d1ce7246e7d9891320b32
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-09-30 17:07:26 +0300

    refactor: centralize API base URL in config.ts
    - Moved the base URL to a single config file for better maintainability.
    - Applied changes to both categoriesApi and questionsApi.

--- app/services/categoriesApi.ts
+++ app/services/categoriesApi.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_BASE_URL } from '@/config';
 
 export const categoriesApi = createApi({
   reducerPath: 'categoriesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummy-api-jtg6bessta-ey.a.run.app/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL}),
   keepUnusedDataFor: 300, // Keep cached data for 5 min 
   endpoints: (builder) => ({
     getCategories: builder.query({
--- app/services/questionsApi.ts
+++ app/services/questionsApi.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_BASE_URL } from '@/config';
 
 export const questionsApi = createApi({
   reducerPath: 'questionsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummy-api-jtg6bessta-ey.a.run.app/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
   endpoints: (builder) => ({
     getQuestions: builder.query({
       query: () => 'getQuestions',
