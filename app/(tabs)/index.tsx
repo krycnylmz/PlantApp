5f6b0af6ce216402dfd1e62ffc6e199a55626da8
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-09-30 19:54:35 +0300

    feat:(animation): add fadeIn animation into home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { View, StatusBar, StyleSheet,SafeAreaView,ActivityIndicator,Text } from "react-native";
+import { View, StatusBar, StyleSheet, SafeAreaView, ActivityIndicator, Text } from "react-native";
 import { Redirect } from "expo-router";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import ClearStorageButton from '@/app/components/Dev/ClearStorageButton'
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import ClearStorageButton from '@/app/components/Dev/ClearStorageButton';
 import Header from '@/app/components/Header';
 import Content from '@/app/components/Content';
 
 const HomeScreen = () => {
-
-
   const [loading, setLoading] = useState(true);
   const [firstLaunch, setFirstLaunch] = useState(false);
+  const [allElementsLoaded, setAllElementsLoaded] = useState(false); // Track render state
+
+  const opacity = useSharedValue(0);
 
   useEffect(() => {
     const checkFirstLaunch = async () => {
@@ -24,10 +26,27 @@ const HomeScreen = () => {
     checkFirstLaunch();
   }, []);
 
+  useEffect(() => {
+    if (!loading && allElementsLoaded) {
+      opacity.value = withTiming(1, { duration: 1000 });
+    }
+  }, [loading, allElementsLoaded]);
+
+  const onElementsLoaded = () => {
+    setAllElementsLoaded(true);
+    console.log("All elements have been rendered!");
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   if (loading) {
     return (
-      <View>
-        <ActivityIndicator />
+      <View style={homeStyles.loadingContainer}>
+        <ActivityIndicator size="large" />
         <Text>Loading...</Text>
       </View>
     );
@@ -38,22 +57,29 @@ const HomeScreen = () => {
   }
 
   return (
-    <View style={homeStyles.container}>
+    <Animated.View style={[homeStyles.container, animatedStyle]} onLayout={onElementsLoaded}>
       <StatusBar barStyle="dark-content" backgroundColor="#130F26" />
       <SafeAreaView>
         <Header />
         <Content />
         <ClearStorageButton />
       </SafeAreaView>
-    </View>
+    </Animated.View>
   );
 };
-// Styles
+
 const homeStyles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     backgroundColor: "rgba(251, 250, 250, 1)",
   },
-})
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(251, 250, 250, 1)",
+  },
+});
+
 export default HomeScreen;
