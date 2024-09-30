47f6bc81ce9ec9760f1dc625338c0446761859f3
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-09-30 20:00:51 +0300

    style(animation): add fadein animation into home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { View, StatusBar, StyleSheet,SafeAreaView,ActivityIndicator,Text } from "react-native";
+import { View, StatusBar, StyleSheet, SafeAreaView, ActivityIndicator, Text } from "react-native";
 import { Redirect } from "expo-router";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import ClearStorageButton from '@/app/components/Dev/ClearStorageButton'
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import ClearStorageButton from '@/app/components/Dev/ClearStorageButton';
 import Header from '@/app/components/Header';
 import Content from '@/app/components/Content';
 
 const HomeScreen = () => {
-
-
   const [loading, setLoading] = useState(true);
   const [firstLaunch, setFirstLaunch] = useState(false);
+
+  // Opacity için shared value
+  const opacity = useSharedValue(0);
 
   useEffect(() => {
     const checkFirstLaunch = async () => {
@@ -24,10 +26,24 @@ const HomeScreen = () => {
     checkFirstLaunch();
   }, []);
 
+  useEffect(() => {
+    if (!loading) {
+      // Sayfa yüklendiğinde opacity'yi 1'e geçiş yapıyoruz (fade-in)
+      opacity.value = withTiming(1, { duration: 1000 });
+    }
+  }, [loading]);
+
+  // Animated style for the fade-in effect
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   if (loading) {
     return (
-      <View>
-        <ActivityIndicator />
+      <View style={homeStyles.loadingContainer}>
+        <ActivityIndicator size="large" />
         <Text>Loading...</Text>
       </View>
     );
@@ -38,16 +54,17 @@ const HomeScreen = () => {
   }
 
   return (
-    <View style={homeStyles.container}>
+    <Animated.View style={[homeStyles.container, animatedStyle]}>
       <StatusBar barStyle="dark-content" backgroundColor="#130F26" />
       <SafeAreaView>
         <Header />
         <Content />
         <ClearStorageButton />
       </SafeAreaView>
-    </View>
+    </Animated.View>
   );
 };
+
 // Styles
 const homeStyles = StyleSheet.create({
   container: {
@@ -55,5 +72,12 @@ const homeStyles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: "rgba(251, 250, 250, 1)",
   },
-})
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(251, 250, 250, 1)",
+  },
+});
+
 export default HomeScreen;
